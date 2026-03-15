3eea58448c91ee0b211b429a008aa6b49fe17a6b
rocksDjoao <joaopaulo2006borges@gmail.com>
2026-03-15 15:32:30 -0300

    Add as classes BubbleSort e SelectionSort por inteiras sem modificar as outras classes, algoritmos foram verificados e estão certos.

--- src/main/java/com/BubbleSort.java
+++ src/main/java/com/BubbleSort.java
@@ -2,8 +2,62 @@ package com;
 
 import java.util.List;
 
-public class BubbleSort<T extends Comparable>{
+public class BubbleSort<T extends Comparable> {
+    public List<T> sort(List<T> list) {
+        if (list == null || list.size() < 2) return list;
+
+        boolean tr;
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            tr = false;
+
+            for (int j = 0; j < list.size() - 1 - i; j++) {
+                T current = list.get(j);
+                T next = list.get(j + 1);
+
+                if (current.compareTo(next) > 0) {
+                    list.set(j, next);
+                    list.set(j + 1, current);
+
+                    tr = true;
+                }
+            }
+
+            if (!tr) {
+                break;
+            }
+        }
+
+        return list;
+    }
+
     public List<Card> sortPrintingCards(List<Card> list) {
-        return null;
+        if (list == null || list.size() < 2) return list;
+
+        boolean swapped;
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < list.size() - 1 - i; j++) {
+                Card current = list.get(j);
+                Card next = list.get(j + 1);
+
+                if (current.compareTo(next) > 0) {
+                    list.set(j, next);
+                    list.set(j + 1, current);
+
+                    Util.printCards(list);
+
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+
+        return list;
     }
 }
--- src/main/java/com/SelectionSort.java
+++ src/main/java/com/SelectionSort.java
@@ -3,7 +3,49 @@ package com;
 import java.util.List;
 
 public class SelectionSort<T extends Comparable> {
+    public List<T> sort(List<T> list) {
+        if (list == null || list.size() < 2) return list;
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            int mi = i;
+
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(j).compareTo(list.get(mi)) < 0) {
+                    mi = j;
+                }
+            }
+
+            if (mi != i) {
+                T min = list.get(mi);
+                list.set(mi, list.get(i));
+                list.set(i, min);
+            }
+        }
+
+        return list;
+    }
+
     public List<Card> sortPrintingCards(List<Card> list) {
-        return null;
+        if (list == null || list.size() < 2) return list;
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(j).compareTo(list.get(minIndex)) < 0) {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {
+                Card min = list.get(minIndex);
+                list.set(minIndex, list.get(i));
+                list.set(i, min);
+
+                Util.printCards(list);
+            }
+        }
+
+        return list;
     }
 }
